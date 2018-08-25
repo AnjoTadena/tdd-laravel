de0bb99ebd710429a45b2dd9aafa7d19205b249a
Anjo Tadena <anjo@coderepubliq.com>
2018-08-26 07:04:11 +0800

    Display all replies associated with the thread.

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -12,5 +12,30 @@
             </div>
         </div>
     </div>
+
+    <div class="row justify-content-center" style="margin-top: 25px;">
+        <div class="col-md-8">
+            <hr>
+
+            <h3>{{$thread->replies->count()}} replies...</h3>
+        </div>
+    </div>
+
+    @foreach ($thread->replies as $reply)
+        <div class="row justify-content-center" style="margin-top: 25px;">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">
+                        <a href="#">{{ $reply->owner->name }}</a>
+                        said
+                        {{ $reply->created_at->diffForHumans() }}
+                    </div>
+                    <div class="card-body">
+                        {{ $reply->body }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endforeach
 </div>
 @endsection

c0c1d3abe389ba1f136863b551932302a84cfe37
Anjo Tadena <anjo@coderepubliq.com>
2018-08-25 21:26:18 +0800

    Create thread test.

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -4,16 +4,34 @@ namespace Tests\Unit;
 
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testBasicTest()
-    {
-        $this->assertTrue(true);
-    }
+	use DatabaseMigrations;
+
+  	// A user can view all threads
+	/** @test */
+	public function a_user_can_view_all_threads()
+	{
+		// arrange
+		$thread = factory(\App\Thread::class)->create();
+
+		$response = $this->get('threads');
+
+		// assert
+		$response->assertSee($thread->title);
+
+	}
+
+	// A user can view thread
+	/** @test */
+	public function a_user_can_view_thread()
+	{
+		$thread = factory(\App\Thread::class)->create();
+
+		$response = $this->get('threads/' . $thread->id);
+
+		$response->assertSee($thread->title);
+	}
 }

f633b2189d558a452ee9f91ecfd1a2f188911298
Anjo Tadena <anjo@coderepubliq.com>
2018-08-26 07:09:35 +0800

    Refactor show threads template.

---
+++ resources/views/components/thread-card.blade.php
@@ -0,0 +1,6 @@
+<div class="card">
+    <div class="card-header">{{$thread->title}}</div>
+    <div class="card-body">
+        {{$thread->body}}
+    </div>
+</div>
--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -22,20 +22,7 @@
     </div>
 
     @foreach ($thread->replies as $reply)
-        <div class="row justify-content-center" style="margin-top: 25px;">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">
-                        <a href="#">{{ $reply->owner->name }}</a>
-                        said
-                        {{ $reply->created_at->diffForHumans() }}
-                    </div>
-                    <div class="card-body">
-                        {{ $reply->body }}
-                    </div>
-                </div>
-            </div>
-        </div>
+        @include('threads.reply')
     @endforeach
 </div>
 @endsection

d1ac09aa5e62ffabd0c23b16c1c6edddf566a834
Anjo Tadena <anjo@coderepubliq.com>
2018-08-26 20:01:17 +0800

    Add disable exception handling to our testing so that we can control our exception.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,13 +5,44 @@ namespace Tests;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use App\User;
+use App\Exceptions\Handler;
+use Illuminate\Contracts\Debug\ExceptionHandler;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication, DatabaseMigrations;
 
+    public function setUp()
+    {
+    	parent::setUp();
+
+	    $this->disableExceptionHandling();
+    }
+
     public function signIn($user = null)
     {
     	$this->be($user ? $user : factory(User::class)->create());
+
+    	return $this;
     }
+
+    protected function disableExceptionHandling()
+    {
+        $this->oldExceptionHandler = $this->app->make(ExceptionHandler::class);
+
+        $this->app->instance(ExceptionHandler::class, new class extends Handler {
+            public function __construct() {}
+            public function report(\Exception $e) {}
+            public function render($request, \Exception $e) {
+                throw $e;
+            }
+        });
+    }
+
+    protected function withExceptionHandling()
+    {
+        $this->app->instance(ExceptionHandler::class, $this->oldExceptionHandler);
+        return $this;
+    }
+
 }

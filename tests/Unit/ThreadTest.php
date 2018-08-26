7ce849e4054397f956d7ceb4c4c90314e563663d
Anjo Tadena <anjo@coderepubliq.com>
2018-08-26 08:22:09 +0800

    Implement a_thread_can_add_reply test.

--- tests/Unit/ThreadTest.php
+++ tests/Unit/ThreadTest.php
@@ -5,9 +5,12 @@ namespace Tests\Unit;
 use Tests\TestCase;
 use App\Thread;
 use App\User;
+use Illuminate\Database\Eloquent\Collection;
 
 class ThreadTest extends TestCase
 {
+	protected $thread;
+
 	public function setUp()
 	{
 		parent::setUp();
@@ -18,12 +21,23 @@ class ThreadTest extends TestCase
 	/** @test */
 	public function a_thread_has_a_replies()
 	{
-		$this->assertInstanceOf('Illuminate\Database\Eloquent\Collection', $this->thread->replies);
+		$this->assertInstanceOf(Collection::class, $this->thread->replies);
 	}
 
 	/** @test */
 	public function a_thread_has_a_creator()
 	{
 		$this->assertInstanceOf(User::class, $this->thread->creator);
+	}
+
+	/** @test */
+	public function a_thread_can_add_a_reply()
+	{
+		$this->thread->addReply([
+			'body' => 'foo',
+			'user_id' => 1
+		]);
+		
+		$this->assertCount(1, $this->thread->replies);
 	}
 }

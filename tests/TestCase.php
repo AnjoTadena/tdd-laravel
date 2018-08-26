bd81baa69fa641d82be4a47899da9a7ddd1ebb62
Anjo Tadena <anjo@coderepubliq.com>
2018-08-26 18:29:03 +0800

    Refactor sign method.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,13 +4,14 @@ namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use App\User;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication, DatabaseMigrations;
 
-    public function signIn($user)
+    public function signIn($user = null)
     {
-    	$this->be($user);
+    	$this->be($user ? $user : factory(User::class)->create());
     }
 }

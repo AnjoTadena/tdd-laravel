5b07471e794d8787bc1e9a8928b8098a39ad09fc
Anjo Tadena <anjo@coderepubliq.com>
2018-08-26 19:59:19 +0800

    Change thread controller routes to resource route.

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,9 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/threads', 'ThreadController@index')->name('threads.index');
-Route::post('/threads', 'ThreadController@store')->name('threads.store');
-Route::get('/threads/{thread}', 'ThreadController@show')->name('threads.show');
+Route::resource('/threads', 'ThreadController');
+// Route::get('/threads', 'ThreadController@index')->name('threads.index');
+// Route::post('/threads', 'ThreadController@store')->name('threads.store');
+// Route::post('/threads', 'ThreadController@store')->name('threads.store');
+// Route::get('/threads/{thread}', 'ThreadController@show')->name('threads.show');
 Route::post('/threads/{thread}/replies', 'ReplyController@store')->name('replies.store');

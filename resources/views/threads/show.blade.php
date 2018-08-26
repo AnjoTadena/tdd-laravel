3b2b186e2c8d58047d2b9a9b5e4cbd318122e392
Anjo Tadena <anjo@coderepubliq.com>
2018-08-26 18:06:30 +0800

    Add form reply.

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -25,5 +25,27 @@
     @foreach ($thread->replies as $reply)
         @include('threads.reply')
     @endforeach
+
+    @if (auth()->check())
+        <div class="row justify-content-center" style="margin-top: 25px;">
+            <div class="col-md-8">
+                <form method="POST" action="{{$thread->path() . '/replies'}}" class="form">
+                    {{csrf_field()}}
+                    <div class="form-group">
+                        <textarea name="body" class="form-control" placeholder="Something to say?" rows="5"></textarea>
+                    </div>
+                    <div class="form-group">
+                        <button class="btn btn-primary">Submit</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    @else
+        <div class="row justify-content-center" style="margin-top: 25px;">
+            <div class="col-md-8">
+                <p>Please <a href="{{route('login')}}">login</a> to participate.</p>
+            </div>
+        </div>
+    @endif
 </div>
 @endsection
